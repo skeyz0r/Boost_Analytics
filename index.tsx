7e5c5a43c83d6c6b1270f6d5eb35a886f635eecb
Shon Danvudi <sdanvudi@gmail.com>
2023-12-20 20:14:05 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,19 +1,33 @@
 
-export default async function Boolytics(info:{prisma:any, project:any, path:string})
+export default async function Boolytics(info:{prisma:any, project:any, leads:string})
 {
     const projectId = info.project
-
 
     await info.prisma.web_analytics.update({
         where:{
             projectId:projectId
         },
         data:{
-            leads: {
+            population: {
                 increment: 1, 
               },
         }
     })
+
+
+        if(window.location.pathname.includes(info.leads))
+        {
+            await info.prisma.web_analytics.update({
+                where:{
+                    projectId:projectId
+                },
+                data:{
+                    leads: {
+                        increment: 1, 
+                      },
+                }
+            })
+        }
     
 
     return('')
