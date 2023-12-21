19b119a9010395bfa207c99d7849c4802b9e96f9
Shon Danvudi <sdanvudi@gmail.com>
2023-12-21 15:16:17 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -10,7 +10,7 @@ export default async function Boolytics(info:{prisma:any, project:any, status:bo
     }
     else
     {
-        Population(info.prisma, info.project)
+    Population(info.prisma, info.project)
     }
 
     return('')
