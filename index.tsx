bc692a1888dd444cacfda047171139da8ed625df
Shon Danvudi <sdanvudi@gmail.com>
2023-12-21 14:50:37 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,10 @@
 import {Leads} from "./lead"
-import {Population} from "./population"
 
 export default async function Boolytics(info:{prisma:any, project:any, status:boolean})
 {
-   if(info.status)
-   {
-   Population(info.prisma, info.project)
-   }
-   else
-   {
+ 
     Leads(info.prisma, info.project)
-   }
+   
 
     return('')
 }
