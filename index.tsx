e15bbce56b437a6c8e4a5295121a2fb21ef5fece
Shon Danvudi <sdanvudi@gmail.com>
2023-12-23 18:43:57 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,33 +1,89 @@
+const today =  Date.now()
+
+   
    export async function lead(prisma:any, project:any)
     {
         const projectId = project
-    
+        
+        const date = await prisma.web_analytics.findMany({
+            where:{projectId:projectId},
+        select:{
+            date: true
+        }
+    })
+    const week = new Date(date[0].date);
+
+    const newDate = new Date()
+
+    if(Number(date[0].date.setDate(week.getDate() + 7)) <= today)
+        {
+            await prisma.web_analytics.update({
+                where:{
+                    projectId:projectId
+                },
+                data:{
+                    leads: 1,
+                      date: newDate,
+                    leadsAll:{increment: 1}       
+                }
+            })
+        }
+    else 
+    {
         await prisma.web_analytics.update({
             where:{
                 projectId:projectId
             },
             data:{
                 leads: {
-                    increment: 1, 
-                  },
+                    increment: 1,
+                },
+                leadsAll:{increment: 1}
             }
         })
     }
-  
+        }
+      
   
    export async function population(prisma:any, project:any)
     {
         const projectId = project
     
+     
+        const date = await prisma.web_analytics.findMany({
+            where:{projectId:projectId},
+        select:{
+            date: true
+        }
+    })
+    const week = new Date(date[0].date);
+
+    const newDate = new Date()
+    
+        if(Number(date[0].date.setDate(week.getDate() + 7)) <= today)
+        {
         await prisma.web_analytics.update({
             where:{
                 projectId:projectId
             },
             data:{
-                population: {
-                    increment: 1, 
-                  },
+                population: 1,
+                populationAll: {increment: 1},
+                date: newDate
             }
         })
     }
-  
+    else
+    {
+        await prisma.web_analytics.update({
+            where:{
+                projectId:projectId
+            },
+            data:{
+                population: {increment: 1},
+                populationAll: {increment: 1}
+            }
+        })
+    }
+    }
+
