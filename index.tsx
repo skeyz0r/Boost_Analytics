fa3e3910e14cdfceef8f6e254807e41087f78c4f
Shon Danvudi <sdanvudi@gmail.com>
2023-12-23 23:02:36 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -31,9 +31,7 @@ export async function lead(prisma:any, project:any)
     })
     const week = new Date(date[0].date);
     
-    console.log('Thesss', Number(date[0].date.setDate(week.getDate() - 1)) <= today)
-    
-        if(Number(date[0].date.setDate(week.getDate() - 1)) <= today)
+        if(Number(date[0].date.setDate(week.getDate() + 7)) <= today)
         {
         await prisma.web_analytics.update({
             where:{
